e6758e0682397976fb40752db81d71674e2bab45
imzhyke <75247669+imzhyke@users.noreply.github.com>
2021-03-26 22:35:05 +0800

    Final Naa

--- billing/billing/billing.aspx.cs
+++ billing/billing/billing.aspx.cs
@@ -9,32 +9,63 @@ namespace Billing
 {
     public partial class Billing : System.Web.UI.Page
     {
+   
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            dateTxtB.Text = DateTime.Now.ToString("MM/dd/yyyy"); 
 
         }
 
         protected void submitBtn_Click(object sender, EventArgs e)
         {
             Table1.Visible = true;
+            cancelBtn.Enabled = true;
 
+            double TAX = 0.0895;
             int qtyTvSold = Convert.ToInt32(tvSoldTxtB.Text);
             int qtyWPSold = Convert.ToInt32(waterPurSoldTxtB.Text);
             int qtyDVDSold = Convert.ToInt32(dvdSoldTxtB.Text);
             int qtyACSold = Convert.ToInt32(airconSoldTxtB.Text);
 
             double tvAmountSold, watPurAmountSold, dvdAmountSold, airConAmountSold;
+            double subTotal, taxAmount, totalP;
 
             tvAmountSold = qtyTvSold * 12000.25;
             watPurAmountSold = qtyWPSold * 2550.90;
             dvdAmountSold = qtyDVDSold * 3520.50;
             airConAmountSold = qtyACSold * 16420.75;
 
-            tvQty.Text = qtyTvSold.ToString();
-            watPurQty.Text = qtyWPSold.ToString();
-            dvdQty.Text = qtyDVDSold.ToString();
-            airConQty.Text = qtyACSold.ToString();
+            subTotal = tvAmountSold + watPurAmountSold + dvdAmountSold + airConAmountSold;
+            taxAmount = subTotal * TAX;
+            totalP = subTotal + taxAmount;
+
+            tvQty.Text = qtyTvSold.ToString("#,#.#");
+            watPurQty.Text = qtyWPSold.ToString("#,#.#");
+            dvdQty.Text = qtyDVDSold.ToString("#,#.#");
+            airConQty.Text = qtyACSold.ToString("#,#.#");
+
+            tvTotalPrice.Text = tvAmountSold.ToString("#,#.#");
+            warPurTotalPrice.Text = watPurAmountSold.ToString("#,#.#");
+            dvdTotalPrice.Text = dvdAmountSold.ToString("#,#.#");
+            airConTotalPrice.Text = airConAmountSold.ToString("#,#.#");
+
+            subPrice.Text = subTotal.ToString("#,#.#");
+            TaxPrice.Text = taxAmount.ToString("#,#.#");
+            totalPrice.Text = totalP.ToString("#,#.#");
+        }
+
+        protected void cancelBtn_Click(object sender, EventArgs e)
+        {
+            Table1.Visible = false;
+            cancelBtn.Enabled = false;
+
+            tvSoldTxtB.Text = "";
+            waterPurSoldTxtB.Text = "";
+            dvdSoldTxtB.Text = "";
+            airconSoldTxtB.Text = "";
+            nameTxtB.Text = "";
+            addTxtB.Text = "";
+            contactNumTxtB.Text = "";
         }
     }
 }
